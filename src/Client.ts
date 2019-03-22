c20f0917920cae745a08fb80f3953fafce31d584
Roland Awemo <awemo@publicplan.de>
2019-03-22 20:36:08 +0100

    ♻  set the default api version

--- src/Client.ts
+++ src/Client.ts
@@ -6,6 +6,7 @@ export class Client {
   private readonly trackingEndpoint: string;
   private readonly siteId: number;
   private readonly rec: number = 1;
+  private readonly apiVersion: string = '1';
 
   constructor(trackingEndpoint: string, siteId: number) {
     this.trackingEndpoint = trackingEndpoint;
@@ -13,7 +14,10 @@ export class Client {
   }
 
   public async track(parameters: TrackingParameters): Promise<void> {
-    const postParameters: any = TrackingParametersConverter.convert(parameters);
+    const postParameters: any = TrackingParametersConverter.convert({
+      apiVersion: this.apiVersion,
+      ...parameters,
+    });
     axios.post(this.trackingEndpoint, {
       idsite: this.siteId,
       rec: this.rec,
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -6,22 +6,35 @@ jest.mock('axios');
 const trackingEndpoint = 'https://example.com/matomo.php';
 const siteId = 1;
 describe('Client', () => {
+  const parameters: TrackingParameters = {
+    actionName: 'push_it',
+    url: 'https://example.com',
+    uniqueUserId: '3b99e3e0759811e8',
+    randomString: '905e6838-4c8e-11e9-8646-d663bd873d93',
+  };
   let client: MatomoClient;
   beforeEach(() => {
     jest.resetModules();
+
     client = new MatomoClient(trackingEndpoint, siteId);
 
   });
-
   it('posts tracks', async () => {
-    const parameters: TrackingParameters = {
-      actionName: 'push_it',
-      url: 'https://example.com',
-      uniqueUserId: '3b99e3e0759811e8',
-      randomString: '905e6838-4c8e-11e9-8646-d663bd873d93',
-      apiVersion: '1',
-    };
+    await client.track({apiVersion: '1', ...parameters});
 
+    const expectedPostParameters: any = {
+      idsite: siteId,
+      rec: 1,
+      action_name: 'push_it',
+      url: 'https://example.com',
+      _id: '3b99e3e0759811e8',
+      rand: '905e6838-4c8e-11e9-8646-d663bd873d93',
+      apiv: '1',
+    };
+    expect(axios.post).toHaveBeenCalledWith(trackingEndpoint, expectedPostParameters);
+  });
+
+  it('sets the default api version', async () => {
     await client.track(parameters);
 
     const expectedPostParameters: any = {
